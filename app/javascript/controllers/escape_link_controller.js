cb69e0e4c43748ba9bad8158e80ef1f7b38b54cd
Jacob Walcutt <jwalcutt22@gmail.com>
2026-09-03 15:49:26 -0400

    Follow the back link when Escape is pressed

---
+++ app/javascript/controllers/escape_link_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Follows the link this controller is attached to when Escape is pressed
+// anywhere on the page. Wire it up with:
+//   data-controller="escape-link" data-action="keydown.esc@window->escape-link#follow"
+export default class extends Controller {
+  follow(event) {
+    event.preventDefault()
+    this.element.click()
+  }
+}
